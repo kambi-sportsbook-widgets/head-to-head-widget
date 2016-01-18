410fa9e811727ddb06713c1059d4702605a17d37
Andrei Teodorescu <andrei@globalmouth.com>
2016-01-18 10:56:27 +0100

    Cleaning

--- src/js/controllers/appController.js
+++ src/js/controllers/appController.js
@@ -12,35 +12,19 @@
             }));
 
             $scope.defaultArgs = {
-               'poll_api_url': 'http://127.0.0.1:3000/api'
             };
 
             /**
              * Keep the height to 0 until we get data
              * @type {number}
              */
-            $scope.defaultHeight = 0;
-
+            $scope.defaultHeight = 450;
 
             /**
              * Listener for page:info, we get here the event id from pageParam
              */
             $scope.$on('PAGE:INFO', function(e, data) {
-               $scope.defaultArgs.poll_group_id = parseInt(data.pageParam);
             });
-
-            /**
-             * Retrieve and assign to scope the group name
-             * @param groupId
-             * @returns {*}
-             */
-            $scope.getGroupName = function ( groupId ) {
-               return kambiAPIService.getGroupById(groupId).then(function ( response ) {
-                  $scope.groupName = response.data.group.name;
-               }, function ( response ) {
-                  console.warn('Failed to fetch group: ' + groupId);
-               });
-            };
 
             /**
              * Request the page info in order to obtain the event id
@@ -51,7 +35,7 @@
              * Init the controller and get the poll data based on the event id
              */
             $scope.init().then(function () {
-               $scope.getPoll($scope.defaultArgs.poll_group_id);
+
             });
          }]);
    })(angular.module('headToHeadWidget'));
