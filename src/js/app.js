91a3b18b3a7d5a5f32114b8a9823dd2b6681fabc
Andrei Teodorescu <andrei@globalmouth.com>
2016-04-11 16:23:55 +0200

    KSBWI-214 Added team  colors

--- src/js/app.js
+++ src/js/app.js
@@ -3,46 +3,82 @@
    'use strict';
 
    var HeadToHead = Stapes.subclass({
-      constructor: function ( name ) {
+      constructor: function () {
          this.scope = {};
+         var appEl = document.getElementById('app');
 
          CoreLibrary.init()
             .then(function ( widgetArgs ) {
+
                this.scope.args = {
                   title: 'Head to Head',
-                  eventId: '1002788405'
+                  eventId: '1003143574'
                };
+
+               this.scope.teamColors = {};
 
                Object.keys(widgetArgs).forEach(function ( key ) {
                   this.scope.args[key] = widgetArgs[key];
                }.bind(this));
 
+               CoreLibrary.widgetModule.enableWidgetTransition(true);
+
                // Setting the args.eventId as a fallback
                var eventId = CoreLibrary.pageInfo.pageParam;
-               if (!CoreLibrary.pageInfo.pageParam) {
+               if ( !CoreLibrary.pageInfo.pageParam ) {
                   eventId = this.scope.args.eventId;
-                  console.log('Missing pageParam, eventId set from args');
+                  console.warn('Missing pageParam, eventId set from args');
                }
 
                CoreLibrary.statisticsModule.getStatistics('h2h', 'event/' + eventId + '/')
-                  .then(function (data) {
+                  .then(function ( data ) {
                      this.scope.data = this.parseDataInfo(data);
                      this.scope.stats = data.lastEvents;
                      this.adjustHeight();
-                  }.bind(this));
+
+                     CoreLibrary.offeringModule.doRequest('/betoffer/live/event/' + eventId + '.json?lang=en_GB&market=GB', false, 'v2')
+                        .then(function ( data ) {
+                           if ( data && data.events && data.events.length ) {
+                              var i = 0, arrLength = data.events.length;
+                              for ( ; i < arrLength; ++i ) {
+                                 var item = data.events[i];
+                                 // offering api returns invalid teamColors hex value, gonna check for length, else we display mock colors
+                                 if ( item.teamColors && (item.teamColors.home.shirtColor1.length === 7 || item.teamColors.home.shirtColor1.length === 4) ) {
+                                    this.scope.teamColors = item.teamColors;
+                                 } else {
+                                    this.scope.teamColors = {
+                                       home: {
+                                          shirtColor1: '#00FFFF',
+                                          shirtColor2: '#FFFFFF'
+                                       },
+                                       away: {
+                                          shirtColor1: '#CC0000',
+                                          shirtColor2: '#000'
+                                       }
+                                    };
+                                 }
+                                 rivets.bind(appEl, this.scope);
+                              }
+                           }
+                        }.bind(this));
+
+                  }.bind(this)).catch(function ( error ) {
+                  console.warn('Cannot load statistics data');
+                  CoreLibrary.widgetModule.setWidgetHeight(0);
+               });
 
             }.bind(this))
-         .catch(function ( error ) {
-            console.debug('init error');
-            console.trace(error);
-         });
+            .catch(function ( error ) {
+               console.debug('init error');
+               console.trace(error);
+            });
 
-         this.view = rivets.bind(document.getElementById('main'), this.scope);
+         rivets.bind(appEl, this.scope);
       },
 
       adjustHeight: function () {
-         var headerHeight = 37;
-         var tableItemHeight = 42;
+         var headerHeight = 37 * 2;
+         var tableItemHeight = 35;
          var contentHeight = headerHeight;
 
          this.scope.stats.forEach(function () {
@@ -72,7 +108,7 @@
             };
 
             // Matching the scores of previous events with the current home team
-            if (homeTeam === item.homeParticipant) {
+            if ( homeTeam === item.homeParticipant ) {
                item.scoreOnLeft = item.homeScore;
                item.scoreOnRight = item.awayScore;
             } else {
@@ -84,8 +120,8 @@
          });
 
          // Iterating through the scores and pushing them to the allScores array
-         for (var i = 0; i < data.lastEvents.length; i++) {
-            for (var j = 0; j < data.lastEvents[i].scores.length; j++) {
+         for ( var i = 0; i < data.lastEvents.length; i++ ) {
+            for ( var j = 0; j < data.lastEvents[i].scores.length; j++ ) {
                allScores.push(data.lastEvents[i].scores[j].homeScore);
                allScores.push(data.lastEvents[i].scores[j].awayScore);
             }
@@ -106,6 +142,6 @@
       }
    });
 
-   var HeadToHead = new HeadToHead();
+   var headToHead = new HeadToHead();
 
 })();
